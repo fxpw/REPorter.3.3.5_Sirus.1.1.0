5dabf7335cdf5cc1ea0c9d181a100ed3c726cb44
fxpw <fxpw@outlook.com>
2023-03-25 18:50:36 +0300

    first commit

---
+++ REPorter/Locals/ruRU.lua
@@ -0,0 +1,28 @@
+if GetLocale() ~= "ruRU" then return end
+
+--Translation by Yabi01 and Keotjii
+REPorterLocale:CreateLocaleTable({
+	Attack   = "Атака",
+	Clear    = "Чисто",               -- BTN
+	Front    = "Спереди",
+	East     = "Восток",
+	West     = "Запад",
+	Guard    = "Нужна защита",        -- Needs review
+	Help     = "Помощь",              -- BTN
+	Incoming = "Нужна помощь против",
+	Losing   = "Потеря точки",        -- Needs review
+	victory  = "победа",
+	["Add \"[REPorter]\" to start of each report"] = "Добавлять \"[REPorter]\" перед сообщениями.",
+	["Heavily defended"] = "Хорошо защищенная", -- Needs review
+	["Lock map"] = "Зафиксировать карту",
+	["Map alpha"] = "Прозрачность карты",
+	["Map scale"] = "Масштаб карты",
+	["New version released!"] = "Вышла новая версия!",
+	["players in area"] = "Союзников в области: ",
+	["Report status"] = "Доложите статус",
+	["Show report bar above map"] = "Показывать над картой",
+	["This addon work only on battlegrounds."] = "Данный аддон работает только на бг.",
+	["This location don't have name. Action canceled."] = "У локации нет имени. Действие прекращено",
+	["To cap"] = "До захвата",
+	["Need help"] = "Нужна помощь"
+});
